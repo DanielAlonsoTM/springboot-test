4a4a0ec16ce7ae378ef600a323b2efbcfd773abc
Manuel Caballero <macaballeropardo@gmail.com>
2024-02-03 12:47:15 -0400

    Inicio Proyecto Sping

---
+++ TestDanielin/src/main/java/TestDanielin/TestDanielin/TestDanielinApplication.java
@@ -0,0 +1,12 @@
+package TestDanielin.TestDanielin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDanielinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestDanielinApplication.class, args);
+	}
+}
---
+++ TestDanielin/src/test/java/TestDanielin/TestDanielin/TestDanielinApplicationTests.java
@@ -0,0 +1,13 @@
+package TestDanielin.TestDanielin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDanielinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
